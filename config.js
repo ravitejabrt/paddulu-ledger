f58ccc4876390cfdee19368e59a4d3904767afeb
ravitejabrt <ravitejabrt@gmail.com>
2026-07-16 18:01:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+ * Paddulu Ledger - Supabase Configuration Credentials
+ * 
+ * Instructions:
+ * -------------------------------------------------------------
+ * 1. You can hardcode your Supabase project credentials below.
+ * 2. Alternatively, you can configure these settings inside the web application UI
+ *    by clicking the "Database Settings" button in the header.
+ * -------------------------------------------------------------
+ */
+
+window.SUPABASE_URL = "https://utcnirtefwvozweyyxen.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_C3TS5j4rClApDlXIHtz8Fw_c55ZKzao";
